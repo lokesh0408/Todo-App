e71078f1547f0492c981a19a97cd8907b5edf760
Lokesh Kumar <lokeshjindal997@gmail.com>
2024-08-13 16:45:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAumq_zyL0AYiRnhqEzSYvpGqWzXa8CZno",
-    authDomain: "todo-971f6.firebaseapp.com",
-    projectId: "todo-971f6",
-    storageBucket: "todo-971f6.appspot.com",
-    messagingSenderId: "200495966128",
-    appId: "1:200495966128:web:01dcc4c7cbdd21e187d875",
-    measurementId: "G-Q45FRCZBRG"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
